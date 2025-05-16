9cdae5c19a205a8519df2945c53e1b1c6b9df335
Samizy29 <onisamuel73@gmail.com>
2025-05-16 09:01:52 +0100

    recheck wk2

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,22 +1,13 @@
-// Toggle the mobile menu
-const menuButton = document.getElementById('menu');
-const navigation = document.querySelector('.navigation');
+// Display current year
+document.getElementById("year").textContent = new Date().getFullYear();
 
-menuButton.addEventListener('click', () => {
-navigation.classList.toggle('show');
+// Display last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
 
-  // Toggle the button icon between ☰ and X
-if (menuButton.textContent === '☰') {
-    menuButton.textContent = '✖';
-} else {
-    menuButton.textContent = '☰';
-}
+// Responsive menu toggle
+const menuButton = document.getElementById("menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+  navigation.classList.toggle("show");
 });
-
-// Set the current year in the footer
-const yearSpan = document.getElementById('year');
-yearSpan.textContent = new Date().getFullYear();
-
-// Set the last modified date
-const lastModifiedSpan = document.getElementById('lastModified');
-lastModifiedSpan.textContent = document.lastModified;
